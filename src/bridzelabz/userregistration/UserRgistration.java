428f20b80465f9b2bd4af0c44e2d0b76c5ad2e85
yami <yaminichaudhari2001@gmail.com>
2023-07-06 16:22:40 +0530

    UC2

--- src/bridzelabz/userregistration/UserRgistration.java
+++ src/bridzelabz/userregistration/UserRgistration.java
@@ -1,7 +1,45 @@
 package bridzelabz.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRgistration {
+    public void validfirstname(){
+        Scanner scanner=new Scanner(System.in);
+        Pattern pattern=Pattern.compile("^[A-Z]+[a-zA-Z]{2,}[0-9]*$");
+        System.out.println("enter the firstname");
+        String firstname=scanner.nextLine();
+        Matcher matcher= pattern.matcher(firstname);
+        if (matcher.hasMatch())
+        {
+            System.out.println("firstname valid");
+        }
+        else
+        {
+            System.out.println("not valid");
+        }
+    }
+    public void validlastname(){
+        Scanner scanner=new Scanner(System.in);
+        Pattern pattern=Pattern.compile("^[A-Z]+[a-zA-Z]{2,}[0-9]*$");
+        System.out.println("enter the lastname");
+        String firstname=scanner.nextLine();
+        Matcher matcher= pattern.matcher(firstname);
+        if (matcher.hasMatch())
+        {
+            System.out.println("lastname valid");
+        }
+        else
+        {
+            System.out.println(" lastname not valid");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("welcom to the User Registration");
+        UserRgistration ob=new UserRgistration();
+        ob.validfirstname();
+        ob.validlastname();
     }
 }
